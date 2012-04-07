63c8cf58fe8eb20401fc8a4e9cd8916ef4241858
Eren Halici <halici.eren@gmail.com>
2012-04-07 11:59:57 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Matsoz::Application.config.secret_token = '673320342b4295dade529af8c1c4fd5f3ddb1e40233e3fa2df4e97340d80f3684adca3559ee8448f2c74a67d080655e3fc9a1d3aefd16c32b2e26c3e9ef34148'
