182b0e5a08016bd9f454f7d40422b54e9de4f401
Eren Halici <halici.eren@gmail.com>
2012-05-14 15:52:04 +0300

    denemeler.js

--- app/assets/javascripts/animations/denemeler.js
+++ app/assets/javascripts/animations/denemeler.js
@@ -1,26 +1,28 @@
 //Bütün kodlar animationInıt fonksiyonunun içinde olacak
 function animationInit(){
-	center_x = canvasWidth/2 , center_y =  canvasHeight/2;
-	x2 = canvasWidth/2 + 100, y2 = canvasHeight/2;
-	x3 = canvasWidth/2 + 100, y3 = canvasHeight/2;
-	aci = 0;
-	yaricap = 50;
-	//çizgi çizdirmek
-	line1=Line.create(center_x,center_y,x2,y2);
-	
-	//çember çizdir
-	circle=Circle.create(70,70,yaricap);
-	
-	//Üçgenin üç ucu için de koordinat gir
-	triangle=Triangle.create(center_x,center_y, center_x/2 - 50,center_y - 50, center_x/2+50,center_y/2-50);
-	triangle.fillStyle='blue'; //mavi üçgen oldu
-	triangle.movable=true; // şimdi üçgen hareket ediyor
-	//dikdörtgen çiz, sol üst köşesinin koordinatları, genişlik ve uzunluk gir
-	rectangle=Rectangle.create(50,150,100,50);
+
+
+
+
+
+	circle=Circle.create(70,70,15);
+
+	circle.onMouseDown = function (x, y) {
+		if (this.contains(x,y)) {
+			this.fillStyle = 'blue';
+			scene.redraw();
+		}
+		// 
+		Movable.onMouseDown.call(this, x, y);
+	}
+
+
+//	circle.movable = true;
 	
 	scene.addDrawable(circle);
-	scene.addDrawable(line1);
-	scene.addDrawable(triangle);
-	scene.addDrawable(rectangle);
-	
+
+	circle2 = Circle.create(100,100,15);
+	scene.addDrawable(circle2);
+	circle2.movable=true;
+
 }
