a1d1bb37c41895e3f09f38fd0549d3d4e2233c3b
Eren Halici <halici.eren@gmail.com>
2012-05-24 16:42:15 +0300

    Rotation works better for mirroring

--- app/assets/javascripts/animations/denemeler.js
+++ app/assets/javascripts/animations/denemeler.js
@@ -1,25 +1,32 @@
 //Bütün kodlar animationInıt fonksiyonunun içinde olacak
 function animationInit(){
-	rect = Rectangle.create(70, 70, 200, 200);
-	rect.movable=true;
-	rect.rotatable=true;
-	rect.fillStyle = 'teal';
-	scene.addDrawable(rect);
+	// rect = Rectangle.create(70, 70, 200, 200);
+	// rect.movable=true;
+	// rect.rotatable=true;
+	// rect.fillStyle = 'teal';
+	// scene.addDrawable(rect);
+	// 
+	// rect2 = Rectangle.create(20, 20, 100, 100);
+	// rect2.movable=true;
+	// rect2.rotatable=true;
+	// rect2.fillStyle = 'teal';
+	// rect.addChild(rect2);
+	// 
+	// triangle = Triangle.create(200,200,300,300,100,300);
+	// triangle.movable = true;
+	// triangle.rotatable = true;
+	// triangle.fillStyle = 'teal';
+	// scene.addDrawable(triangle);
+	// 
+	// arc = Sector.create(300, 200, 30, 0, Math.PI/3, true);
+	// arc.movable = true;
+	// arc.rotatable = true;
+	// scene.addDrawable(arc);
 	
-	rect2 = Rectangle.create(20, 20, 100, 100);
-	rect2.movable=true;
-	rect2.rotatable=true;
-	rect2.fillStyle = 'teal';
-	rect.addChild(rect2);
-	
-	triangle = Triangle.create(200,200,300,300,100,300);
-	triangle.movable = true;
-	triangle.rotatable = true;
-	triangle.fillStyle = 'teal';
-	scene.addDrawable(triangle);
-	
-	arc = Sector.create(300, 200, 30, 0, Math.PI/3, true);
-	arc.movable = true;
-	arc.rotatable = true;
-	scene.addDrawable(arc);
+	label = Label.create(100, 100, "R");
+	label.setFontSize(96);
+	label.movable = true;
+	label.rotatable = true;
+	label.setScale(-1, 1);
+	scene.addDrawable(label);
 }
