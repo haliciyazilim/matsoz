a0ee9b09cf4af2a616a1aa6cfb4d35ddb8c79723
unknown <yunus_work@OFIS-8.(none)>
2012-06-28 18:01:54 +0300

    Path class is extended with some shapes

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -24,21 +24,86 @@ Main.init = function(){
 		Main.raphaelInit();
 		Interaction.init(Main.interaction);
 	} else if (framework == 'paper') {
-		Main.paperInit();
-		Main.scale = 2;
+		
+		Main.scale = 1;
 		paper.install(window);
+		Main.paperInit();
 		width = 512;
 		height = 320;
 		Main.interaction.innerHTML = "<canvas id='interaction_canvas' class='interaction_canvas' width='"+width*Main.scale+"px' height='"+height*Main.scale+"px'></canvas>"
 		canvas = $('.interaction_canvas').get(0);
 		paper.setup(canvas);
-		
+		AnimationManager();
 		Interaction.init(Main.interaction);
 	}
 };
 
 Main.paperInit = function() {
-
+	Path.Triangle = function(p1,p2,p3){
+		var triangle = new Path();
+		triangle.add([p1.x,p1.y]);
+		triangle.add([p2.x,p2.y]);
+		triangle.add([p3.x,p3.y]);
+		triangle.closed = true;
+		return triangle;
+	}
+	Path.Bowl = function(x,y,w,h){
+		//this.path('M'+x+','+y+'L'+(x+w)+','+y+'L'+(x+w*0.8)+','+(y+h)+'L'+(x+0.2*w)+','+(y+h)+' z');
+		var bowl = new Path();
+		bowl.add([x,y]);
+		bowl.add([x+w,y]);
+		bowl.add([x+w*0.8,y+h]);
+		bowl.add([x+w*0.2,y+h]);
+		bowl.closed = true;
+		return bowl;
+	};
+	Path.Rhomboid = function(p,s,_w){
+		var x=p.x,y=p.y,w=s.width,h=s.height;
+		var rhomboid = new Path();
+		rhomboid.add([x+_w,y]);
+		rhomboid.add([x+_w+w,y]);
+		rhomboid.add([x+w,y+h]);
+		rhomboid.add([x,y+h]);
+		rhomboid.closed = true;
+		return rhomboid;
+	}
+	Path.Cube = function(p,a){
+		var x=p.x,y=p.y;
+		var _x=x+a*0.3,_y=y+a*0.3;
+		var cube = new Path();
+		cube.add([x	,_y]);
+		cube.add([x	,_y+a]);
+		cube.add([x+a  ,_y+a]);
+		cube.add([x+a  ,_y]);
+		cube.add([x	,_y]);
+		cube.add([_x   ,y]);
+		cube.add([_x+a ,y]);
+		cube.add([x+a  ,_y]);
+		cube.add([x+a  ,_y+a]);
+		cube.add([_x+a ,y+a]);
+		cube.add([_x+a ,y]);
+		cube.add([x+a	,_y])
+		cube.closed = true;
+		return cube;
+	}
+	Path.Rhombus = function(p,s){
+		var x=p.x,y=p.y,w=s.width,h=s.height;
+		/*
+		var pathstring = '';
+		pathstring += 'M'+x+','+(y+h*0.5);
+		pathstring += 'L'+(x+w*0.5)+','+(y);
+		pathstring += 'L'+(x+w)+','+(y+h*0.5);
+		pathstring += 'L'+(x+w*0.5)+','+(y+h);
+		pathstring += 'z';
+		*/
+		var rhombus = new Path();
+		rhombus.add([x,y+h*0.5]);
+		rhombus.add([x+w*0.5,y]);
+		rhombus.add([x+w,y+h*0.5]);
+		rhombus.add([x+w*0.5,y+h]);
+		rhombus.closed = true;
+		return rhombus;
+	}
 };
 
 Main.raphaelInit = function(){
@@ -302,3 +367,61 @@ var Util = {
 			return Math.floor(Math.random()*2);
 		}
 };
+
+var AnimationManager = function(){
+	AnimationManager.animations = [];
+	console.log('AnimationManager is initialized');
+	view.onFrame = AnimationManager.onFrame;
+}
+
+AnimationManager.onFrame = function(event){
+	for(var i=0; i<AnimationManager.animations.length ; i++){
+		var anim = AnimationManager.animations[i];
+			
+		if(anim.startTime == null){
+			anim.startTime = event.time;
+			
+		}
+		var animate = false;
+		anim.count++;
+		anim.lastTime=event.time;
+		switch(anim.type){
+			case 'translate':
+				var x,y;
+				x  = anim.data.first_position.x + anim.data.delta.x*(event.time - anim.startTime);
+				y  = anim.data.first_position.y + anim.data.delta.y*(event.time - anim.startTime);
+				//console.log(anim.data.first_position.x, anim.data.delta.x,event.time , anim.startTime)
+				anim.shape.position = [x,y];
+				break;
+		}
+			console.log(event.time,anim.startTime,anim.time);
+		if(event.time - anim.startTime > anim.time/1000){
+			anim.shape.position.x = anim.data.first_position.x + anim.data.delta.x * anim.time / 1000;
+			anim.shape.position.y = anim.data.first_position.y + anim.data.delta.y * anim.time / 1000;
+			AnimationManager.animations.splice(i,1);
+			
+		}
+		
+	}
+}
+AnimationManager.translate = function(shape,delta,time){
+	
+	var anim = new Animation('translate');
+	anim.shape = shape;
+	anim.data.first_position = {x:shape.position.x,y:shape.position.y};
+	anim.data.delta = {};
+	anim.data.delta.x = delta.x/time*1000;
+	anim.data.delta.y = delta.y/time*1000;
+		
+	console.log(anim.data.first_position)
+	anim.time = time;
+	anim.startTime = null;
+	anim.lastTime = null;
+	AnimationManager.animations.push(anim);
+}
+function Animation(type){
+	this.type = type;
+	this.shape = null;
+	this.time = null;
+	this.data = {};
+}
