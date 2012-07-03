be8f6d7e65def40733998d5c69a5232fc7dc8b3a
unknown <yunus_work@OFIS-8.(none)>
2012-07-03 17:50:41 +0300

    converted to paper.js and some modification has been done

--- app/assets/javascripts/animations/cember.js
+++ app/assets/javascripts/animations/cember.js
@@ -1,34 +1,52 @@
 // JavaScript Document
 
 /*Styles*/
-var triangleStyle = {
-	'fill': '#f55',
-	'stroke-width':'2px'
-};
+
 var textStyle = {
 	'font-size':'16px',
 	'text-color': '#55f'
 };
-var edgeStyle = {
-	'stroke-width':'2px'
+var lineStyle = {
+	strokeColor:"#000",
+	strokeWidth:1
+}
+var circleStyle = {
+	strokeColor:"#000"
 };
 var angleStyle = {
 	'stroke-width': '2px'
 };
-var rulerStyle = {
-	'fill' : '#fff',
-};
-var rulerTextStyle = {
-};
-var rulerLineStyle = {
-};
+
+
 /*Styles*/
 var Interaction = function(){}; Interaction();
+Interaction.getFramework = function() {
+	return 'paper';
+}
+Interaction.images = [
+	{
+		id: "ruler",
+		src: '/assets/animations/ruler.png'
+	},
+	{
+		id: "compass_left_leg",
+		src: '/assets/animations/compass_left_leg.png'
+	},
+	{
+		id: "compass_knuckle",
+		src: '/assets/animations/compass_knuckle.png'
+	},
+	{
+		id: "compass_right_leg",
+		src: '/assets/animations/compass_right_leg.png'
+	}
+];
+
 Interaction.init = function(container){
 	Main.setObjective('Aşağıdaki cetvel ve pergeli kullanarak seçeceğiniz yarıçap uzunluğuna sahip çemberi aşağıdaki “Çiz” düğmesine tıklayarak çiziniz.');
+
 	Interaction.container = container;
-	$(container).css('position','relative')
-	Interaction.paper = new Raphael(container,$(container).width(),$(container).height());
+	Interaction.paper = {width:$(container).width(),height:$(container).height()};
 	Interaction.button = document.createElement('input');
 	Interaction.button.type = "button";
 	Interaction.button.value = 'Çiz';
@@ -48,147 +66,250 @@ Interaction.init = function(container){
 			width:'30px',
 			height:'30px'
 		});
-	Main.setObjective('start');
+		
 	Interaction.container.appendChild(Interaction.radius);
-	Interaction.reinit();
-}
-
-Interaction.pause = false;
-Interaction.reinit = function(){
-	Interaction.paper.clear();
+	
+	Interaction.status = document.createElement('div');
+	Interaction.status.className = "status_true";
+	$(Interaction.status).css({
+		position:'absolute',
+		left:Interaction.paper.width*0.05,
+		top:Interaction.paper.height*0.6,
+		width:'40%'
+	});
+	Interaction.container.appendChild(Interaction.status);
+				
 	Interaction.r = null;
 	Interaction.drawRuler();
-	Interaction.compassLayer = Interaction.paper.rect(0,0,20,20).attr({'cursor':'e-resize','fill':'#fff','opacity':1});
-	Interaction.drawCompass();
-	var start = function () {
-		//if(Interaction.pause === true)
-		//	return;
-		Main.setObjective('start');
-        this.ox  = this.attr('x')+10;
-		this.mx  = this.data('mx');
-	},
-    move = function (dx) {
-		//if(Interaction.pause === true)
-		//	return;
-		Main.setObjective('move');
-		var _w= - this.mx + (this.ox + dx);
-		;
-		if(_w > Interaction.br * 3 && _w < Interaction.rulerSet[0].data('w')-Interaction.br){
-			Interaction.compassSet.remove()
-			Interaction.drawCompass(_w);
-		}
-    },
-    up = function (){
-		
-		Main.setObjective('up');
-    };
-	Interaction.compassLayer.drag(move,start,up);
-	
-}
+	Interaction.initCompass();
+};
+
+Interaction.pause = false;
 
 Interaction.setRadius = function(r){
 	Interaction.r = r;
-	Interaction.radius.innerHTML = Util.formatNumber(r/Interaction.br,2);
-}
+	Interaction.radius.innerHTML = Util.numberTurkishFloating(r/Interaction.br,1);
+};
 
 Interaction.drawCircle = function(){
 	if(Interaction.pause === true)
 		return;
+	Interaction.status.innerHTML = "";
+				
 	Interaction.pause = true;
 	var x,y,xc,yc,xr,yr,R,_o;
 	Interaction.drawCircle.x = Interaction.paper.width*0.65;
 	Interaction.drawCircle.y = Interaction.paper.height*0.55;
-	R = Interaction.rulerSet[0].data('w')*0.7;;
+	
 	if(Interaction.t)
 		clearInterval(Interaction.t)
 	if(Interaction.drawCircle.compass)
 		Interaction.drawCircle.compass.remove();
-	Interaction.drawCircle.compass = Interaction.compass(Interaction.drawCircle.x,Interaction.drawCircle.y,R,Math.acos(Interaction.r/(2*R)));
+	Interaction.drawCircle.compass = new Compass(Interaction.drawCircle.x,Interaction.drawCircle.y);
+	Interaction.drawCircle.compass.changeDelta(Interaction.r-Interaction.drawCircle.compass.right.bounds.width);
+		Interaction.drawCircle.compass.group.opacity=0;
+	Interaction.drawCircle.compass.group.animate({
+		style:{
+			opacity:1
+		},
+		duration:500
+	});
+	Interaction.drawCircle.t = setInterval(
+		function(){
+			var dt = new Date().getTime() - Interaction.animationStarted;
+			
+			Interaction.drawCircle._o = dt * 0.22;
+			
+			if( Interaction.drawCircle._o >= 360 ){
+				clearTimeout(Interaction.drawCircle.t);
+				Interaction.drawCircle._o = 359.9;
+				Interaction.pause = false;
+				Interaction.drawCircle.textO = new PointText(new Point(Interaction.drawCircle.x-10,Interaction.drawCircle.y+10));
+				Interaction.drawCircle.textO.content = "O";
+				Interaction.drawCircle.textR = new PointText(new Point(Interaction.drawCircle.x+Interaction.r*0.2,Interaction.drawCircle.y+15));
+				Interaction.drawCircle.textR.content = "r = "+(Interaction.radius.innerHTML);
+				Interaction.drawCircle.lineR = new Path.Line(new Point(Interaction.drawCircle.x,Interaction.drawCircle.y),new Point(Interaction.drawCircle.x+Interaction.r,Interaction.drawCircle.y));
+				Interaction.drawCircle.lineR.setStyle(lineStyle);
+				Interaction.status.innerHTML = "O merkezli ve "+(Interaction.radius.innerHTML)+" birim yarıçaplı çember ";
+				
+				//Interaction.drawCircle.group = new Group();
+				//Interaction.drawCircle.group.addChild(Interaction.drawCircle.textO);
+				//Interaction.drawCircle.group.addChild(Interaction.drawCircle.textR);
+				//Interaction.drawCircle.group.addChild(Interaction.drawCircle.lineR);
+				Interaction.drawCircle.textO.opacity=0;
+				Interaction.drawCircle.textO.animate({
+					style:{
+						opacity:1
+					},
+					duration:1000
+				});
+				
+				Interaction.drawCircle.textR.opacity=0;
+				Interaction.drawCircle.textR.animate({
+					style:{
+						opacity:1
+					},
+					duration:1000
+				});
+				Interaction.drawCircle.lineR.opacity=0;
+				Interaction.drawCircle.lineR.animate({
+					style:{
+						opacity:1
+					},
+					duration:1000
+				});
+				Interaction.drawCircle.compass.group.opacity=1;
+				Interaction.drawCircle.compass.group.animate({
+					style:{
+						opacity:0
+					},
+					duration:500
+				});
+			}
+				
+			var _x = Interaction.drawCircle.x + Interaction.r * Math.cos(Util.degreeToRadians(Interaction.drawCircle._o));
+			var _y = Interaction.drawCircle.y + Interaction.r * Math.sin(Util.degreeToRadians(Interaction.drawCircle._o));
+			if(Interaction.drawCircle.circle)
+				Interaction.drawCircle.circle.remove();
+			var center = new Point(Interaction.drawCircle.x,Interaction.drawCircle.y);
+			
+			var radius = Interaction.r;
+			var startAngle = Util.degreeToRadians(0);
+			var endAngle = Util.degreeToRadians(Interaction.drawCircle._o);
+			
+			Interaction.drawCircle.compass.rotate(-Interaction._o_old+Interaction.drawCircle._o, center);
+			var point1 = new Point(center.x + Math.cos(startAngle) * radius,
+							   center.y + Math.sin(startAngle) * radius);
+			var point2 = new Point(center.x + Math.cos((startAngle+endAngle)/2) * radius,
+								   center.y + Math.sin((startAngle+endAngle)/2) * radius);
+			var point3 = new Point(center.x + Math.cos(endAngle) * radius,
+								   center.y + Math.sin(endAngle) * radius);
+			Interaction.drawCircle.circle = new Path.Arc(point1, point2, point3);
+			Interaction.drawCircle.circle.setStyle(circleStyle);
+			Interaction.drawCircle.circle.opacity = Interaction.drawCircle._o>200 ? 1 :Interaction.drawCircle._o/200; 
+			Interaction._o_old = Interaction.drawCircle._o;
+			Interaction.drawCircle.circle.moveBelow(Interaction.drawCircle.compass.group);
+		},
+		1 
+	);
+	
 	if(Interaction.drawCircle.textO)
 		Interaction.drawCircle.textO.remove();
 	if(Interaction.drawCircle.textR)
 		Interaction.drawCircle.textR.remove();
-	Interaction._o = 20;
+	if(Interaction.drawCircle.lineR)
+		Interaction.drawCircle.lineR.remove();
+	Interaction.drawCircle._o = 20;
 	Interaction.animationStarted = new Date().getTime();
-	Interaction.t = setInterval(function(){
-		Interaction._o = (new Date().getTime() - Interaction.animationStarted) * 0.36;
-		if( Interaction._o >= 360 ){
-			clearTimeout(Interaction.t);
-			Interaction.drawCircle.compass.remove();
-			Interaction.drawCircle.textO = Interaction.paper.text(Interaction.drawCircle.x-10,Interaction.drawCircle.y+10, "O").attr(textStyle);
-			Interaction.drawCircle.textR = Interaction.paper
-												.text(Interaction.drawCircle.x+Interaction.r*0.5,Interaction.drawCircle.y+10,"r = "+Interaction.radius.innerHTML);
-			Interaction._o = 359.9;
-			Interaction.pause = false;
-		}
-		if(Interaction._o >= 180){
-			var fa = 1;
-			var fs = 1;
-		}
-		else if(Interaction._o < 180) {
-			var fa = 0;
-			var fs = 1;
-		}
-		var _x = Interaction.drawCircle.x + Interaction.r * Math.cos(Util.degreeToRadians(Interaction._o));
-		var _y = Interaction.drawCircle.y + Interaction.r * Math.sin(Util.degreeToRadians(Interaction._o));
-		if(Interaction.drawCircle.circle)
-			Interaction.drawCircle.circle.remove();
-		Interaction.drawCircle.circle = Interaction.paper.path(
-									'M'+Interaction.drawCircle.x+','+Interaction.drawCircle.y+
-									'L'+(Interaction.drawCircle.x+Interaction.r)+','+Interaction.drawCircle.y+
-									'A'+Interaction.r+','+Interaction.r +
-									' 0 '+fa+','+fs+' '+_x+','+_y);
-		for(var i=0;i<Interaction.drawCircle.compass.length; i++)
-			Interaction.drawCircle.compass[i].transform('r'+Interaction._o+','+Interaction.drawCircle.x+','+Interaction.drawCircle.y);
+	Interaction._o_old =0;	
+};
 
-	},1 );
-}
+function Compass(x,y){
+	this.defaultX = x;
+	this.defaultY = y;
+	this.left = new Raster('compass_left_leg');
+	this.right = new Raster('compass_right_leg');
+	this.knuckle = new Raster('compass_knuckle');
+	this.R = this.left.size.height;
+	this.group = new Group();
+	this.group.addChild(this.left);
+	this.group.addChild(this.right);
+	this.group.addChild(this.knuckle);
+	this.left.position = [
+		x,
+		y-this.left.size.height*0.5
+	]; 
+	this.knuckle.position = [
+		x+this.left.size.width*0.5,
+		y-this.left.size.height-this.knuckle.size.height*0.5
+	];
+	this.right.position = [
+		x+this.left.size.width,
+		y-this.right.size.height*0.5
+	];
 
-Interaction.compass = function(x,y,R,_o){
-	var st = Interaction.paper.set();
-	st.push(Interaction.paper.line(x,y,x+R*Math.cos(_o)*0.2,y+R*Math.sin(_o)*0.2));
-	st.push(Interaction.paper.line(x+R*Math.cos(_o)*0.2,y+R*Math.sin(_o)*0.2,x+R*Math.cos(_o),y+R*Math.sin(_o)).attr('stroke-width',10));
-	st.push(Interaction.paper.line(x+2*R*Math.cos(_o),y,x+R*Math.cos(_o),y+R*Math.sin(_o)).attr({'stroke-width':10}));
-	st.push(Interaction.paper.circle(x+R*Math.cos(_o),y+R*Math.sin(_o),R*0.1).attr('fill','#fff'));
-	st.push(Interaction.paper.line(x+R*Math.cos(_o),y+R*(Math.sin(_o)+0.1),x+R*Math.cos(_o),y+R*(Math.sin(_o)+0.2)).attr('stroke-width',10));
-	return st;
-}
-Interaction.drawCompass = function(d){
-	var x,y,xc,yc,xr,yr,R,_o;
-	if(d==null || d=='undefined')
-		d = Interaction.rulerSet[0].data('w')*0.6;
-	Interaction.setRadius(d);
-	x = Interaction.rulerSet[0].data('x');
-	y = Interaction.rulerSet[0].data('y')+Interaction.rulerSet[0].data('h');
-	R = Interaction.rulerSet[0].data('w')*0.7;
-	_o= Math.acos(d/(2*R));
-	Interaction.compassSet = Interaction.compass(x,y,R,_o);
-	Interaction.compassLayer.toFront();
-	Interaction.compassLayer.attr('x',x+2*R*Math.cos(_o)-11);
-	Interaction.compassLayer.attr('y',y)
-	Interaction.compassLayer.data({
-			 	'x':x+2*R*Math.cos(_o)-20,
-				'y':y,
-				'mx':x,
-				'my':y
-			});
-}
+	this.d = this.right.position.x -this.left.position.x;
+	
+	this.remove = function(){
+		this.left.remove();
+		this.right.remove();
+		this.knuckle.remove();
+	};
+	this.changeDelta = function(dx){
+		var _betha = Math.acos((this.d)/(2*this.R));
+		var _alpha = Math.acos((this.d+dx)/(2*this.R));
+		var _o = Util.radianToDegree(_betha ) - Util.radianToDegree(_alpha);
+		var dy  = this.R*(Math.sin(_betha) - Math.sin(_alpha));
+		this.left.rotate(
+			_o,
+			[
+				this.defaultX,
+				this.defaultY
+			]
+		);
+		this.right.rotate(
+			-_o,
+			[
+				this.defaultX+this.d,
+				this.defaultY
+			]
+		);
+		this.knuckle.position.y += dy;
+		this.right.position.x += dx;
+		this.knuckle.position.x += dx/2;
+		this.d += dx;
+	};
+	this.rotate = function(angle,point){
+		this.left.rotate(angle,point);
+		this.right.rotate(angle,point);
+		this.knuckle.rotate(angle,point);
+	}
+};
+
+Interaction.initCompass = function(){
+	Interaction.compass = new Compass(Interaction.ruler.bounds.x,Interaction.ruler.bounds.y);
+	Interaction.compass.right.class = "right_leg";
+
+	Interaction.drawCompass(Interaction.br*2);
+	var tool = new Tool();
+	tool.drag = false;
+	tool.onMouseDown = function(event){
+		if(Interaction.compass.group.bounds.contains(event.point))
+			this.drag=true;
+	}
+	tool.onMouseDrag = function(event){
+		if(this.drag === true && Interaction.pause == false){
+			Interaction.drawCompass(event.delta.x);
+		}
+	}
+	tool.onMouseUp = function(){
+		this.drag = false;
+	}
+	tool.activate();
+};
+
+Interaction.drawCompass = function(dx){
+	if(dx == null || dx == undefined)
+		dx = 0;
+	if( Interaction.compass.d + dx > Interaction.br*9 ||
+		Interaction.compass.d + dx < Interaction.br)
+		return;
+	Interaction.compass.changeDelta(dx)
+	Interaction.setRadius(Interaction.compass.d);
+};
 
 Interaction.drawRuler = function(){
 	var x,y,w,h,b,st;
-	Interaction.rulerSet = Interaction.paper.set();
-	x = Interaction.paper.width*0.1;
-	y = Interaction.paper.height*0.15;
-	w = Interaction.paper.width*0.20;
-	Interaction.br = Math.floor(w*0.1);
-	h = Interaction.paper.height*0.1;
-	Interaction.rulerSet.push(Interaction.paper.rect(x-Interaction.br*0.5,y,w,h).attr(rulerStyle).data({'x':x,'y':y,'w':w,'h':h}));
+	x = Interaction.paper.width*0.02;
+	y = Interaction.paper.height*0.4;
+	Interaction.ruler = new Raster('ruler');
+	Interaction.ruler.position = [
+		x+Interaction.ruler.size.width*0.5,
+		y+Interaction.ruler.size.height*0.5
+	];
+	Interaction.br = Math.floor(Interaction.ruler.size.width*0.1);
+
 	var _y1 = y+h*0.6;
 	var _yt = y+h*0.4;
-	var _y2 = y+h; 
-	for(var i=0; i<10; i++){
-		var _x = x+Interaction.br*(i);
-		Interaction.rulerSet.push(Interaction.paper.line(_x,_y1,_x,_y2).attr(rulerLineStyle).attr({'x':_x,'y':_y1}));
-		Interaction.rulerSet.push(Interaction.paper.text(_x,_yt,i).attr(rulerTextStyle).attr({'x':_x,'y':_yt}));
-	}
-}
+	var _y2 = y+h;
+};
