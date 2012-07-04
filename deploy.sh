3eff80c72a20cfeeed820f1ec4472bdf4349ea6b
Eren Halici <halici.eren@gmail.com>
2012-07-04 18:30:22 +0300

    Modified deploy.sh to remove precompiled assets in development branch

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -d "public/assets" ]; then
+    # Control will enter here if $DIRECTORY exists.
+	git rm -rf public/assets
+	git commit -m "Auto-deletion of precompiled assets"
+fi
 git checkout master
 git merge development
 rm -rf public/assets
