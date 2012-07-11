0b751b0bc2565fa86abbc5d03fbf46bb6af95ba6
unknown <yunus_work@OFIS-8.(none)>
2012-07-11 17:55:05 +0300

    example animation is finished

--- app/assets/javascripts/animations/duzgun_cokgen.js
+++ app/assets/javascripts/animations/duzgun_cokgen.js
@@ -5,6 +5,7 @@ var textStyle = {fontSize:16,strokeColor:'#fff',strokeWidth:0,fillColor:'#fff'};
 var edgeStyle = {'stroke-width':'2px'};
 var angleStyle = {'fill':'#DDD'};
 //var shapeStyle = {'fill':'#fff','shape-rendering':'crispEdges'};
+var animationShapeStyle = {strokeColor:'#000',strokeWidth:2}
 var shapeStyle = {strokeColor:'#000',strokeWidth:2,fillColor:'#fff'};
 var dropableShapeHoverStyle = {strokeColor:'#000',fillColor:'#dd9',strokeWidth:2};
 var dropableShapeDefaultStyle = {strokeColor:'#999',fillColor:'rgb(146,208,80)',strokeWidth:1};
@@ -13,35 +14,32 @@ var dropableShapeDroppedFalseStyle = {strokeColor:'#f00',fillColor:'#f00'};
 //Styles
 
 var Animation = {
+	images:[
+		{
+			id:'triangle_pattern',
+			src:'/assets/animations/duzgun_cokgen/triangle.png'
+		},
+		{
+			id:'square_pattern',
+			src:'/assets/animations/duzgun_cokgen/square.png'
+		},
+		{
+			id:'pentagon_pattern',
+			src:'/assets/animations/duzgun_cokgen/pentagon.png'
+		},
+		{
+			id:'hexagon_pattern',
+			src:'/assets/animations/duzgun_cokgen/hexagon.png'
+		}
+	],
 	init: function(container){
 		Animation.container = container;
 		var w=$(container).width(), h=$(container).height();
 		var x = w *0.5;
 		var y = h*0.5;
-		
-		var p1 = new Point(
-			w*0.125   ,
-			20
-		);
-		var p2 = new Point(
-			x*0.375 ,
-			20 
-		);
-		var p3 = new Point(
-			x*0.625,
-			20
-		);
-		var p4 = new Point(
-			x*0.875,
-			20 
-		);
-		
-		var animationHelper = {
-			animate: Item.prototype.animate,
-			t1:60,
-			t2:180,
-			t3:330,
-		}
+		var _h = 10;
+		var size = new Size(150,170);
+		//console.log(size)
 		function PolygonInterface(P,S,angles){
 			this.setAngles = function(angles){
 				if(angles instanceof Array)
@@ -52,28 +50,233 @@ var Animation = {
 			this.draw = function(){
 				if(this.shape)
 					this.shape.remove();
-				this.shape = new Path.EquiradialPolygon(this.center,this.size,this.angles);
-				this.shape.setStyle(shapeStyle)
+				this.shape = new Path.EquiradialPolygon(this.position,this.size,this.angles,0);
+				this.shape.setStyle(animationShapeStyle);
 			}
 			this.redraw = function(angles){
 				this.setAngles(angles);
 				this.draw();
 			}
 			
-			this.center = P;
+			this.position = P;
 			this.size = S
 			
 			if(angles){
 				this.setAngles(angles);
 			}
 		}
+		var p1 = new Point(
+			0,
+			27+0.5-_h
+		);
+		var p2 = new Point(
+			w*0.25 ,
+			12-_h 
+		);
+		var p3 = new Point(
+			w*0.5,
+			4-Math.floor(_h)
+		);
+		var p4 = new Point(
+			w*0.75,
+			0-_h 
+		);
 		
-		Animation.triangle = new PolygonInterface(p1,new Size(w*0.20,h),[animationHelper.t1,animationHelper.t2,animationHelper.t3]);
-		//console.log(p1)
+		var animationHelper = {
+			animate: Item.prototype.animate,
+			t1:150,
+			t2:210,
+			t3:330,
+			s1:30,
+			s2:150,
+			s3:210,
+			s4:330,
+			p1:60,
+			p2:90,
+			p3:120,
+			p4:200,
+			p5:330,
+			h1:30,
+			h2:60,
+			h3:150,
+			h4:200,
+			h5:210,
+			h6:350
+		}
+		
+		Animation.triangle = new PolygonInterface(
+			p1,
+			size,
+			[
+				animationHelper.t1,
+				animationHelper.t2,
+				animationHelper.t3
+			]
+		);
 		Animation.triangle.draw();
-		//new Path.EquiradialPolygon(p1,new Size(w*0.20,h),[animationHelper.t1,animationHelper.t2,animationHelper.t3]);
-		//new Path.Line(new Point(10,10),new Point(700,100))
-		//console.log('asdasd')
+		Animation.square = new PolygonInterface(
+			p2,
+			size,
+			[
+				animationHelper.s1,
+				animationHelper.s2,
+				animationHelper.s3,
+				animationHelper.s4
+			]
+		);
+		Animation.square.draw();
+		
+		Animation.pentagon = new PolygonInterface(
+			p3,
+			size,
+			[
+				animationHelper.p1,
+				animationHelper.p2,
+				animationHelper.p3,
+				animationHelper.p4,
+				animationHelper.p5
+			]
+		);
+		Animation.pentagon.draw();
+		
+		Animation.hexagon = new PolygonInterface(
+			p4,
+			size,
+			[
+				animationHelper.h1,
+				animationHelper.h2,
+				animationHelper.h3,
+				animationHelper.h4,
+				animationHelper.h5,
+				animationHelper.h6
+			]
+		);
+		Animation.hexagon.draw();
+
+		Animation.onFrame = function(event){
+			Animation.triangle.redraw([
+				animationHelper.t1,
+				animationHelper.t2,
+				animationHelper.t3
+			]);
+			Animation.square.redraw([
+				animationHelper.s1,
+				animationHelper.s2,
+				animationHelper.s3,
+				animationHelper.s4
+			]);
+			Animation.pentagon.redraw([
+				animationHelper.p1,
+				animationHelper.p2,
+				animationHelper.p3,
+				animationHelper.p4,
+				animationHelper.p5
+			]);
+			Animation.hexagon.redraw([
+				animationHelper.h1,
+				animationHelper.h2,
+				animationHelper.h3,
+				animationHelper.h4,
+				animationHelper.h5,
+				animationHelper.h6
+			]);
+		}
+
+		
+		animationHelper.animate({
+			style:{t1:90},
+			duration:1750,
+			delay:100,
+			animationType: 'easeInEaseOut',
+			callback:function(){
+				
+				console.log('Im here');
+				$(Animation.container).append('<span style="position:absolute;bottom:10px;left:40px">Eşkenar Üçgen</span>');
+				var raster = new Raster('triangle_pattern');
+				raster.position = Animation.triangle.shape.centerPoint;
+				raster.opacity = 0;
+				raster.animate({
+					style:{opacity:1},
+					duration:1000,
+					delay:500
+				});
+			}
+		});
+		animationHelper.animate({
+			style:{
+				s1:45,
+				s2:135,
+				s3:225,
+				s4:315
+			},
+			duration:1500,
+			delay:200,
+			animationType: 'easeInEaseOut',
+			callback:function(){
+				console.log('Im here');
+				$(Animation.container).append('<span style="position:absolute;bottom:10px;left:34%">Kare</span>');
+				var raster = new Raster('square_pattern');
+				raster.position = Animation.square.shape.centerPoint;
+				raster.opacity = 0;
+				raster.animate({
+					style:{opacity:1},
+					duration:1000,
+					delay:500
+
+				});
+			}
+		});
+		animationHelper.animate({
+			style:{
+				p1:18,
+				p2:90,
+				p3:162,
+				p4:234,
+				p5:306,
+			},
+			duration:2000,
+			delay:250,
+			animationType: 'easeInEaseOut',
+			callback:function(){
+				console.log('Im here');
+				$(Animation.container).append('<span style="position:absolute;bottom:10px;left:53.5%">Düzgün Beşgen</span>');
+				var raster = new Raster('pentagon_pattern');
+				raster.position = Animation.pentagon.shape.centerPoint;
+				raster.opacity = 0;
+				raster.animate({
+					style:{opacity:1},
+					duration:1000,
+					delay:500
+
+				});
+			}
+		});
+		animationHelper.animate({
+			style:{
+				h1:0,
+				h2:60,
+				h3:120,
+				h4:180,
+				h5:240,
+				h6:300
+			},
+			duration:2500,
+			animationType: 'easeInEaseOut',
+			callback:function(){
+				console.log('Im here');
+				$(Animation.container).append('<span style="position:absolute;bottom:10px;left:78%">Düzgün Altıgen</span>');
+				var raster = new Raster('hexagon_pattern');
+				raster.position = Animation.hexagon.shape.centerPoint;
+				raster.opacity = 0;
+				raster.animate({
+					style:{opacity:1},
+					duration:1000,
+					delay:500
+
+				});
+			}
+		});
+		
 	}
 };
 var Interaction =function(){};Interaction();
@@ -82,21 +285,23 @@ Interaction.getFramework = function() {
 }
 
 Interaction.init = function(container){
-	Main.setObjective('Aşağıdaki çokgenlerden düzgün olanları seçiniz.');
+	Main.setObjective('Yandaki çokgenlerden düzgün olanları seçiniz.');
 	Interaction.container = container;
 	Interaction.container.top = $(container).offset().top;
 	Interaction.container.left = $(container).offset().left;
 	var w = $(Interaction.container).width();
 	var h = $(Interaction.container).height();
+	Interaction.shuffledArray = null;
 	project.activeLayer.removeChildren();
-	Interaction.createDropableShape(w*0.8,0,w*0.2,h);
-	Interaction.generateRandomShapes(w*0.05,h*0.2,w*0.7,h*0.8);
+	Interaction.createDropableShape(w*0.75,0,w*0.2,h);
+	Interaction.shapeCount = 0;
+	Interaction.generateRandomShapes(20,20,w*0.8,h-20);
 	Interaction.paper = {width:500,height:300};
 	Interaction.preventDrag = false;
 	if(Interaction.status == null || Interaction.status == 'undefined'){
 		Interaction.status = document.createElement('div');
 		Interaction.status.className = 'status_true';
-		$(Interaction.status).css({'position':'absolute','top':''+(h-20)+'px','left':'0px','padding-left':'20px'});
+		$(Interaction.status).css({'position':'absolute','top':''+(h-40)+'px','left':'0px','padding-left':'20px'});
 		Interaction.container.appendChild(Interaction.status);
 	}
 	else
@@ -210,64 +415,89 @@ var start = function(){
 	};
 Interaction.generateRandomShapes = function(X,Y,WIDTH,HEIGHT){
 	Interaction.shapes = [];
-	var maxW = WIDTH*0.2;
+	var maxW = WIDTH*0.25;
 	var maxH = HEIGHT*0.3;
 	do{///generate shapes randomly
 		var x,y,w,h;
 		var p = Interaction.findSpace(WIDTH,HEIGHT);
 		x = p.x+X, y = p.y+Y;
-		Interaction.shapeType = Math.floor(Math.random()*6);
-		//Interaction.shapeType = 2;
+		
 		w = maxW*0.7;
-		h = maxH*0.7;
-		var shape = {};
+		h = maxH*0.8;
+		var shape;
 		var isRegular;
-		if(Util.rand01() == 0)
-			isRegular = true;
-		else
-			isRegular = false;
+		var NUMBER_OF_SHAPES  = 12;
+		Interaction.shapeCount = Interaction.shapeCount%NUMBER_OF_SHAPES;
+		if(Interaction.shuffledArray == null || Interaction.shuffledArray == undefined)
+			Interaction.shuffledArray = Util.getShuffledArray(NUMBER_OF_SHAPES);
+		Interaction.shapeType = Interaction.shuffledArray[Interaction.shapeCount++];
+		//console.log(Interaction.shuffledArray)
 		switch(Interaction.shapeType){
 			case 0:
+				var a = Math.min(w,h);
+				shape = new Path.Rectangle(new Point(x+a*0.15,y+a*0.15),new Size(a*0.7,a*0.7));
+				isRegular = true;
+				break;
+			case 1:
 				h = w = Math.min(w,h);
-				if(isRegular == false){
-					while(h == w || h > maxH)
-						h = Math.floor(Math.random()*2)*10+w-20;
-				}
-				shape = new Path.Rectangle(new Point(x,y),new Size(w,h));
+				while(h == w || h > maxH)
+					h = -Math.floor(Math.random()*3)*30+w+30;
+				shape = new Path.Rectangle(new Point(x+10,y+10),new Size(w,h));
+				isRegular = false;
 				break;
 				
-			case 1:
-				var a,b,c;
-					a = b = c = 5;
-				if(isRegular == false){
-					if(Util.rand01() == 0)
-						a = 4, b = 3;
-					else
-						a = 3, b = 4;
-				}	
-				shape = new Triangle(a,b,c,x,y,w,h);
-				break;
 			case 2:
-				if(isRegular == false)
-					shape = pentagon(new Point(x,y), new Size(w,h));
-				else
-					shape = regularpentagon(new Point(x,y), new Size(w,h));
+				var a,b,c;
+				a = b = c = 5;	
+				shape = new Triangle(a,b,c,x,y,w,h);
+				isRegular = true;
 				break;
+			
 			case 3:
-				if(isRegular == false)
-					shape = hexagon(new Point(x,y), new Size(w,h));
-				else
-					shape = regularhexagon(new Point(x,y), new Size(w,h));
+				var a,b,c;
+				a = b = c = 5;
+				a = 3, b = 4;
+				shape = new Triangle(a,b,c,x,y,w,h);
+				isRegular = false;
 				break;
+			
 			case 4:
-				if(isRegular == true)
-					continue;
-				shape = new Path.Rhomboid(new Point(x,y+h*0.1), new Size(w*0.8,h*0.7), w*0.2);
+				shape = pentagon(new Point(x,y), new Size(w,h));
+				isRegular = false;
 				break;
 			case 5:
-				if(isRegular == true)
-					continue;
-				shape = new Path.Rhombus(new Point(x,y+h*0.1),new Size(w,h*0.7) );
+				shape = regularpentagon(new Point(x,y), new Size(w,h));
+				isRegular = true;
+				break;
+			case 6:
+				shape = hexagon(new Point(x,y), new Size(w,h));
+				isRegular = false;
+				break;
+			case 7:
+				shape = regularhexagon(new Point(x,y), new Size(w,h));
+				isRegular = true;
+				break;
+			case 8:
+				shape = new Path.Rhomboid(new Point(x,y+h*0.1), new Size(w*0.6,h*0.7), w*0.2);
+				isRegular = false;
+				break;
+			case 9:
+				shape = new Path.Rhombus(new Point(x+w*0.1,y+h*0.1),new Size(w*0.8,h*0.7) );
+				isRegular = false;
+				break;
+			case 10:
+				var a,b,c;
+				a = b = c = 5;
+				a = 6, b = 8;
+				shape = new Triangle(a,b,c,x,y,w,h);
+				isRegular = false;
+				break;
+			case 11:
+				var a,b,c;
+				a = b = c = 5;
+				a = 4, b = 3;
+				shape = new Triangle(a,b,c,x,y,w,h);
+				isRegular = false;
 				break;
 		}
 		shape.isRegular = isRegular;
@@ -276,14 +506,12 @@ Interaction.generateRandomShapes = function(X,Y,WIDTH,HEIGHT){
 		shape.move = move;
 		shape.up = up;
 		shape.callback = function(){
-			//console.log(this)
 			this.preventDrag = false; 
-			//console.log("I'm here");
 			
 		};
 		shape.order = Interaction.shapes.length;
 		Interaction.shapes.push(shape);
-	}while( Interaction.shapes.length < 15 )
+	}while( Interaction.shapes.length < 12 )
 
 };
 
@@ -309,8 +537,8 @@ Interaction.createDropableShape = function(X,Y,WIDTH,HEIGHT){
 Interaction.findSpace = function(w,h){
 	var n = Interaction.shapes.length;
 	var p = {
-			x:Math.floor(n%5)*w*0.2,
-			y:Math.floor(n/5)*h*0.3
+			x:Math.floor(n%4)*w*0.2,
+			y:Math.floor(n/4)*h*0.3
 		};
 	return p;
 }
