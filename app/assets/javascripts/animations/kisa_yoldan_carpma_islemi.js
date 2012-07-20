46a3e2d7cd8b0e80b306af943d9101a7d06f39e6
Yunus Eren Guzelll <yeguzel@halici.com.tr>
2012-07-20 13:09:34 +0300

    kis_yoldan_carpma_islemi.js is finished

---
+++ app/assets/javascripts/animations/kisa_yoldan_carpma_islemi.js
@@ -0,0 +1,288 @@
+// JavaScript Document
+var animationTextStyle = {
+	fontSize:16
+}
+var Animation = {
+
+	init:function(container){
+			function textAnimate(point,content,style,animateStyle,delay,callback){
+				var pT1 = new PointText(point);
+				pT1.content = content;
+				pT1.set_style(animationTextStyle);
+				pT1.set_style(style);
+				pT1.animate({
+					style:animateStyle,
+					duration:1000,
+					delay:delay,
+					callback:callback,
+					animationType:'easeInEaseOut'
+				});
+				return pT1;
+			};
+			Animation.container = container;
+			var p1 = new Point(250.5,50.5);
+			var p2 = new Point(250.5,100.5);
+			var p3 = new Point(250.5,150.5);
+			
+			textAnimate(
+				p1,
+				'5273',
+				{opacity:0},
+				{opacity:1},
+				100
+			);
+			textAnimate(
+				p1.add(50,0),
+				' x 7',
+				{opacity:0},
+				{opacity:1},
+				600
+			);
+			textAnimate(
+				p1.add(102,0),
+				'=',
+				{opacity:0},
+				{opacity:1},
+				600
+			);
+			textAnimate(
+				p1.add(82,0),
+				'0',
+				{opacity:0},
+				{opacity:1},
+				600,
+				function(){
+					this.animate({
+					style:{fillColor:new RgbColor(1,0,0)},
+					duration:500,
+					delay:900,
+					});
+				}
+			)
+			textAnimate(
+				p1.add(122,0),
+				'36911',
+				{opacity:0},
+				{opacity:1},
+				1200
+			);
+			textAnimate(
+				p1.add(82,0),
+				'0',
+				{opacity:0},
+				{opacity:1,position:p1.add(180,0),fillColor:new RgbColor(1,0,0)},
+				3000
+			);
+			
+			var delay = 3000;
+			textAnimate(
+				p2,
+				'8324',
+				{opacity:0},
+				{opacity:1},
+				100+delay
+			);
+			textAnimate(
+				p2.add(50,0),
+				' x 2',
+				{opacity:0},
+				{opacity:1},
+				600+delay
+			);
+			textAnimate(
+				p2.add(112,0),
+				'=',
+				{opacity:0},
+				{opacity:1},
+				600+delay
+			);
+			textAnimate(
+				p2.add(84,0),
+				'00',
+				{opacity:0},
+				{opacity:1},
+				600+delay,
+				function(){
+					this.animate({
+					style:{fillColor:new RgbColor(1,0,0)},
+					duration:500,
+					delay:900,
+					});
+				}
+			)
+			textAnimate(
+				p2.add(132,0),
+				'16648',
+				{opacity:0},
+				{opacity:1},
+				1200+delay
+			);
+			textAnimate(
+				p2.add(84,0),
+				'00',
+				{opacity:0},
+				{opacity:1,position:p2.add(192,0),fillColor:new RgbColor(1,0,0)},
+				3000+delay
+			);
+			
+			
+			var delay = 6000;
+			textAnimate(
+				p3,
+				'4921',
+				{opacity:0},
+				{opacity:1},
+				100+delay
+			);
+			textAnimate(
+				p3.add(50,0),
+				' x 5',
+				{opacity:0},
+				{opacity:1},
+				600+delay
+			);
+			textAnimate(
+				p3.add(122,0),
+				'=',
+				{opacity:0},
+				{opacity:1},
+				600+delay
+			);
+			textAnimate(
+				p3.add(84,0),
+				'000',
+				{opacity:0},
+				{opacity:1},
+				600+delay,
+				function(){
+					this.animate({
+					style:{fillColor:new RgbColor(1,0,0)},
+					duration:500,
+					delay:900,
+					});
+				}
+			)
+			textAnimate(
+				p3.add(142,0),
+				'24605',
+				{opacity:0},
+				{opacity:1},
+				1200+delay
+			);
+			textAnimate(
+				p3.add(84,0),
+				'000',
+				{opacity:0},
+				{opacity:1,position:p3.add(202,0),fillColor:new RgbColor(1,0,0)},
+				3000+delay
+			);
+
+		}
+}
+
+var Interaction = {
+	getFramework:function(){
+			return 'paper';
+		},
+	init:function(container){
+			Interaction.container = container;
+			Main.setObjective('Yandaki çarpma işlemini yapınız ve kontrol ediniz.');
+			Interaction.paper = {
+				width:$(container).width(),
+				height:$(container).height()
+			}
+			
+			Interaction.appendInput({
+				position:'relative',
+				width:'120px',
+				top:'-2px',
+				fontSize:'20px',
+				height:'35px'
+			});
+			$(Interaction.input).attr('maxlength','7')
+			var div = document.createElement('div');
+			$(container).append(div);
+			$(div)
+				.css({
+					position:'absolute',
+					top:100,
+					right:200,
+					lineHeight:'40px',
+					fontSize:'20px',
+					width:'300px',
+					height:'50px',
+					letterSpacing:'1px',
+					textAlign:'right'
+				})
+				.html('<span id="factor1"></span>&nbsp;x&nbsp;<span id="factor2"></span>&nbsp;=&nbsp;')
+				.append(Interaction.input);
+			Interaction.questionDiv = div;
+			Interaction.factor1Span = $('span#factor1',div).get(0);
+			Interaction.factor2Span = $('span#factor2',div).get(0);
+			Interaction.appendButton({
+				bottom:'40px',
+				right:'40px'
+			});
+			Interaction.appendStatus({
+				bottom:'50px',
+				right:'170px'
+			})
+			
+			Interaction.prepareNextQuestion();
+		},
+	nextQuestion: function(){
+			if(Interaction.solutionDiv)
+				$(Interaction.solutionDiv).remove();
+				
+			Interaction.factor1 = Math.floor(Math.random()*10000);
+			Interaction.factor2 = Math.pow(10,Math.floor(Math.random()*3+1))*Math.floor(Math.random()*9+1); 
+			$(Interaction.factor1Span).html(Interaction.factor1);
+			$(Interaction.factor2Span).html(Interaction.factor2);
+			
+		},
+	isAnswerCorrect : function(value){
+			if(value == Interaction.factor1 * Interaction.factor2)
+				return true;
+			else 
+				return false;
+		},
+	onCorrectAnswer : function(){
+			
+		},
+	onWrongAnswer : function(){
+			
+		},
+	onFail : function(){
+			Interaction.pause = true;
+			Interaction.setStatus('Yanlış cevap, doğrusu ' +  Interaction.factor1 * Interaction.factor2 + ' olacaktı',false);
+			Interaction.solutionDiv = $(Interaction.questionDiv).clone().insertAfter(Interaction.questionDiv);
+			var zeros = $(Interaction.factor2Span).html();
+			zeros = '<span class="zero">'+zeros.substring(1,zeros.length)+'</span>';
+			var html = "" + $(Interaction.solutionDiv).html();
+			html = html.substring(0,html.indexOf('<input')) + (Interaction.factor1*Interaction.factor2Span.innerHTML.substring(0,1)) + zeros;
+			$(Interaction.solutionDiv)
+				.html(html)
+				.append('<span id="result"></span>')
+				.css({
+					top:$(Interaction.solutionDiv).position().top+60
+				});
+			$('#factor2',Interaction.solutionDiv)
+				.html(Interaction.factor2Span.innerHTML.substring(0,1)+zeros )
+			$('#factor2 .zero',Interaction.solutionDiv)
+				.css({color:'#000'})
+				.animate(
+					{color:'#f00'},
+					1000
+				);
+			$('.zero',Interaction.solutionDiv)
+				.css({color:'#000'})
+				.delay(500)
+				.animate(
+					{color:'#f00'},
+					1000,
+					function(){
+						Interaction.pause = false;
+					}
+				);
+		},
+}
