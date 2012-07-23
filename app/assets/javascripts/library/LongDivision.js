c7c54010132fb33be80efdba6c234244268f92e7
Yunus Eren Guzelll <yeguzel@halici.com.tr>
2012-07-23 14:27:58 +0300

    indendetion problem of steps are resolved

--- app/assets/javascripts/library/LongDivision.js
+++ app/assets/javascripts/library/LongDivision.js
@@ -35,52 +35,64 @@ function LongDivision(n,d,container){
 	$(this.nodes.dividend).height($(this.nodes.divisor).height()+$(this.nodes.answer).height());
 	
 	$(this.nodes.answer).html(this.answer);
-	this.nodes.steps = [];
-	
-	this.nextStep = function(){
+	this.nodes.steps = [];	
+    
+}
+
+LongDivision.prototype.nextStep = function(duration){
 		if(this.i >= this.numLength)
-			return;
+			return null;
 		var digit = this.i < this.numLength ? parseInt(this.num[this.i],10) : 0;
 		var lastStepDown = ""+this.remainder+digit;
 		var stepDividend = digit + (this.remainder * 10);
 		var stepAnswer = Math.floor(stepDividend/this.d);
 		this.answer = this.answer + stepAnswer;
-		
 		if(this.nodes.steps.length > 0){
-			//lastStepDown += $('.down',this.nodes.steps[this.nodes.steps.length-1]).html();
 			$('.down',this.nodes.steps[this.nodes.steps.length-1]).append(digit);
 		}
 		if(stepDividend < this.d){
 			this.i++;
-			
-			$(this.nodes.answer).html(parseInt(this.answer,10));	
+			if(this.answer > 0)
+				$(this.nodes.answer).append(parseInt(stepAnswer,10));	
 			this.remainder = stepDividend;
-			this.nextStep();
-			return;
+			return this.nextStep(duration);
+			
 		}
 		this.remainder = stepDividend%this.d;
-		var minus  = Math.floor((digit + (this.remainder * 10)));
-		$(this.nodes.answer).html(parseInt(this.answer,10));
+		//var minus  = Math.floor((digit + (this.remainder * 10)));
+		
 		this.i++;
 		var div = document.createElement('div');
-		$(this.container).append(div);
+		
 		$(div)
 			.html('<div class="up"></div><div class="line"><img src="/assets/animations/minus_sign.png" /></div><div class="down"></div>')
 			.css({
 				position:'relative',
 				top:-this.fontSize,
-				left:this.fontSize*0.5*(this.nodes.steps.length),
 				height:this.fontSize*2+2,
 				float:'left',
 				width:$(this.container).width()
 			});
 		$('.up',div)
-			.html(stepAnswer*this.d)
+			.html(''+stepAnswer*this.d)
 			.css({
 				height:this.fontSize,
-				width:this.fontSize,
-				paddingLeft:this.fontSize*0.5*(this.i -this.nodes.steps.length -  $('.up',div).html().length)
+				textAlign:'left',
+				width:this.fontSize*0.5*this.num.length + 1
+								
 			})
+		var pLeft=0;
+		if(this.nodes.steps.length > 0){
+			pLeft = $('.down',this.nodes.steps[this.nodes.steps.length - 1]).html().length 
+						  - $('.up',div).html().length 
+						  + parseInt($(this.nodes.steps[this.nodes.steps.length - 1]).attr('pLeft'),10);
+		}
+		$(div).attr('pLeft',pLeft);
+		
+		console.log("stepDividend "+stepDividend + ": "+(""+stepDividend).length,pLeft,$(div).attr('pLeft'));
+		
+		//$('.up',div).css({paddingLeft:this.fontSize*0.5*paddingLeft});
+		
 		$('.line',div)
 			.css({
 				position:'relative',
@@ -88,7 +100,8 @@ function LongDivision(n,d,container){
 				borderTop:'1px solid #000',
 				width:this.fontSize*0.5*(this.num.length+2),
 				left:-this.fontSize
-			})
+			});
+		
 		$('.line img',div)
 			.css({
 				position:'relative',
@@ -96,15 +109,32 @@ function LongDivision(n,d,container){
 				left:'0px',
 				zIndex:2
 			});
+		var remainderString = "";
+		for(var i=0;i<(""+stepDividend).length - (""+this.remainder).length;i++)
+			remainderString += "0";
+		remainderString += this.remainder;
 		$('.down',div)
-			.html(this.remainder)
+			.html(remainderString)
 			.css({
 				height:this.fontSize,
-				paddingLeft:this.fontSize*0.5*(this.i -this.nodes.steps.length - $('.down',div).html().length)
+				paddingLeft:this.fontSize*0.5*($('.up',div).html().length - $('.down',div).html().length)
 			});
-			
+		
+		$(div).css({left:this.fontSize*0.5*(pLeft)});
+		
+		$(this.container).append(div);
+		if(stepAnswer != undefined)
+			$(this.nodes.answer).append('<span id="'+this.i+'">'+stepAnswer+'</span>');
+		if(!isNaN(duration)){
+			$(div)
+				.delay(duration*0.5)
+				.css({opacity:0})
+				.animate({opacity:1},duration*0.5);
+			$('span#'+this.i,this.nodes.answer)
+				.css({opacity:0})
+				.delay(1)
+				.animate({opacity:1},1);
+		}
 		this.nodes.steps.push(div);
+		return div;
 	}
-	
-    
-}
