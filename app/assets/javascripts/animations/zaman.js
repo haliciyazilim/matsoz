2e86f5f672c4789bc6cce4a8b8ba69d115a2485e
Yunus Eren Guzelll <yeguzel@halici.com.tr>
2012-07-30 18:00:54 +0300

    created

--- app/assets/javascripts/animations/zaman.js
+++ app/assets/javascripts/animations/zaman.js
@@ -37,8 +37,25 @@ var Interaction = {
 			linkTo:function(){
 					Interaction.openPage(Interaction.clockPage);
 				}	
+		},
+		{
+			id: "car",
+			src: '/assets/animations/cizgi_grafigi/car.jpg'
+		},
+		{
+			id: "kadran",
+			src: '/assets/animations/cizgi_grafigi/kadran.png'
+		},
+		{
+			id: "akrep",
+			src: '/assets/animations/cizgi_grafigi/akrep.png'
+		},
+		{
+			id: "yelkovan",
+			src: '/assets/animations/cizgi_grafigi/yelkovan.png'
 		}
 	],
+	
 	getFramework:function(){
 			return 'paper';
 		},
@@ -77,48 +94,85 @@ var Interaction = {
 					arrow.rotate(180,Interaction.centerPoint.add(0,-50));
 					arr.addChild(arrow);
 					arr.addChild(arrow2);
+					Interaction.timeLine = arr;
 					Interaction.yearDivs = [];
 					Interaction.eventDivs = [];
 					var divCss = {
 							'transform':'rotate(-90deg)',
 							'-webkit-transform':'rotate(-90deg)',
+							'-ms-transform':'rotate(-90deg)', /* IE 9 */
+							'-moz-transform':'rotate(-90deg)', /* Firefox */
+							'-o-transform':'rotate(-90deg)', /* Opera */
 							position:'absolute',
 							width:'80px',
 							height:'40px',
 							border:'1px solid #999',
-							overflow:'hidden'
+							overflow:'hidden',
+							resize:'none'
 							
 						}
 					for(var i=0; i<7;i++){
 						
 						arr.addChild(new Path.Circle(startPoint.add(40+i*70,0),5).set_style({fillColor:'#000'}));
 						
-						var yearDiv = document.createElement('div');
+						var yearDiv = document.createElement('textarea');
+						yearDiv.setAttribute('type','text');
 						Interaction.yearDivs.push(yearDiv);
 						$(Interaction.container).append(yearDiv);
 						$(yearDiv)
-							.attr('contenteditable','true')
+							.html('')
+							.attr('contenteditable',true)
 							.css(divCss).css({top:'30px',left:45+i*70,fontSize:'30px'})
 						
-						var eventDiv = document.createElement('div');
+						var eventDiv = document.createElement('textarea');
+						eventDiv.setAttribute('type','text');
 						Interaction.eventDivs.push(eventDiv);
 						$(Interaction.container).append(eventDiv);
 						$(eventDiv)
-							.attr('contenteditable','true')
-							.css(divCss).css({top:'150px',left:30+i*70,width:'110px'})
+							.html('')
+							.attr('contenteditable',true)
+							.css(divCss).css({top:'150px',left:25+i*70,width:'120px'})
 					}
 					$(Interaction.eventDivs[0]).html('Annem ve babam evlendi.')
 					$(Interaction.eventDivs[2]).html('Doğum tarihim.')
 					$(Interaction.eventDivs[4]).html('Okula başladım.')
 				},
 				dispose:function(f){
-				
+					$(Interaction.yearDivs).each(function(index, element) {
+                        $(this).remove();
+                    });
+					$(Interaction.eventDivs).each(function(index, element) {
+                        $(this).remove();
+                    });
+					Interaction.timeLine.remove();
 					
 				}
+				
 			};
 			Interaction.calendarPage = {
 				init:function(){
-				
+					
+					var calendarContainer = document.createElement('div');
+					$(Interaction.container).append(calendarContainer);
+					$(calendarContainer).css({
+						position:'absolute',
+						top:'10px',
+						left:'50%'
+					})
+					var calendarImageContainer = document.createElement('div');
+					$(calendarContainer).append('<style></style>');
+					var calendar = document.createElement('div');
+					$(calendarContainer).append(calendar);
+					$( calendar ).datepicker({firstDay:1});
+					var date = new Date();
+					if(date.getMonth() > 8)
+						date.setMonth(8);
+					else{
+						date.setMonth(8)
+						date.setFullYear(date.getFullYear()-1);
+					
+					}
+					$(calendar).datepicker('setDate',date)
 				},
 				dispose:function(f){
 				
@@ -126,7 +180,19 @@ var Interaction = {
 			};
 			Interaction.clockPage = {
 				init:function(){
-				
+					Interaction.clock = new Clock(new Point(100,100));
+					Interaction.clock.setTime({h:4,m:20});
+					//clock.setTime({h:5,m:20});
+					Interaction.appendButton({
+						bottom:'40px',
+						right:'40px'
+					});
+					Interaction.appendStatus({
+						bottom:'10px',
+						right:'40px'
+					});			
+					Interaction.prepareNextQuestion();
+
 				},
 				dispose:function(f){
 				
@@ -134,10 +200,8 @@ var Interaction = {
 			};
 			
 			
-			Interaction.prepareNextQuestion();
 		},
 	openPage: function(page){
-			console.log('openpage : ' , page)
 			if(page == Interaction._openedPage)
 				return;
 			Interaction._waitingPage = page;
@@ -150,8 +214,6 @@ var Interaction = {
 				Interaction._openedPage.dispose(_openPage)
 			}
 			else{
-				//_openPage();
-				console.log("not opened")
 				$('.menu_link_images').each(function(index, element) {
                     $(this).animate({width:$(this).width()*0.6,bottom:'0px',left:(100+70*index)+'px',marginLeft:-$(this).width()*0.63},1000)
                 }); 
@@ -182,4 +244,63 @@ var Interaction = {
 	onFail : function(){
 		
 		},
+}
+
+
+function Clock(p){
+	this.p = p;
+	this.animating = false;
+	this.kadran = new Raster('kadran');
+	this.kadran.position = this.p;
+	this.yelkovan = new Raster('yelkovan');
+	this.yelkovan.position = this.p;
+	this.akrep = new Raster('akrep');
+	this.akrep.position = this.p;
+	this.akrep.lastTransformation = this.akrep.matrix;
+	this.yelkovan.lastTransformation = this.yelkovan.matrix;
+	this.setTime = function(endTime,startTime){
+		while(this.animating == true);
+		this.animating = true;
+		this.endTime = endTime;
+		this.endTime.h = this.endTime.h%24;
+		this.endTime.m = this.endTime.m%60;
+		if(startTime){
+			startTime.h = startTime.h%24;
+			startTime.m = startTime.m%60;
+		}
+		else if(this.startTime){
+			startTime = this.startTime;
+			this.startTime = undefined;
+		}
+		else{
+			startTime = {h:0,m:0}
+		}
+		this.clockHelper = new AnimationHelper({
+			yelkovanAngle: 360*startTime.h + 6*startTime.m,
+			akrepAngle: 30*startTime.h+startTime.m*0.5,
+			owner:this
+		});
+		this.clockHelper.animate({
+			style: {
+				yelkovanAngle: 360*this.endTime.h + 6*this.endTime.m + (this.endTime.h < this.akrep360*12),
+				akrepAngle: 30*this.endTime.h+this.endTime.m*0.5
+			},
+			duration: Math.abs(this.endTime.h - startTime.h + (this.endTime.m - startTime.m)/60)*400,
+			animationType:'easeInEaseOut',
+			update: function() {
+				var matrix = new Matrix();
+				matrix.rotate(this.akrepAngle, this.owner.p.x, this.owner.p.y);
+				matrix.concatenate(this.owner.akrep.lastTransformation);
+				this.owner.akrep.setMatrix(matrix);
+				matrix = new Matrix();
+				matrix.rotate(this.yelkovanAngle, this.owner.p.x, this.owner.p.y);
+				matrix.concatenate(this.owner.yelkovan.lastTransformation);
+				this.owner.yelkovan.setMatrix(matrix);
+			},
+			callback:function(){
+				this.owner.startTime = this.owner.endTime;
+				this.owner.animating = false;
+			}
+		});
+	}
 }
