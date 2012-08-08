ad7901e566aad3c5563adeba8ae62861b49af757
Yunus Eren Guzelll <yeguzel@halici.com.tr>
2012-08-08 16:53:01 +0300

    modification has been done

--- app/assets/javascripts/animations/kisa_yoldan_carpma_islemi.js
+++ app/assets/javascripts/animations/kisa_yoldan_carpma_islemi.js
@@ -1,9 +1,16 @@
 function __Styles(){
     animationTextStyle = {
-        fontSize:24,
+        fontSize:18,
         fillColor:new RgbColor(1,1,1,0.9),
         font:"cursive"
     }
+	interactionTextStyle = {
+		fontSize:16,
+		fillColor:new RgbColor(0,0,0)
+	}
+    if(navigator.appName == "Microsoft Internet Explorer"){
+		animationTextStyle.font = "arial";
+	}
 }
 
 var Animation = {
@@ -16,9 +23,9 @@ var Animation = {
 
 	init:function(container){
             Animation.container = container;
-			var p1 = new Point(150.5,50.5);
-			var p2 = new Point(150.5,100.5);
-			var p3 = new Point(150.5,150.5);
+			var p1 = new Point(250.5,50.5);
+			var p2 = new Point(250.5,100.5);
+			var p3 = new Point(250.5,150.5);
 			var w=$(container).width(), h=$(container).height();
 			var board = new Raster('board');
 			board.position = new Point(w*0.5,h*0.5+2)
@@ -84,8 +91,8 @@ var Interaction = {
 				$(Interaction.solutionDiv).remove();
 			Interaction.factor1 = Math.floor(Math.random()*10000);
 			Interaction.factor2 = Math.pow(10,Math.floor(Math.random()*3+1))*Math.floor(Math.random()*9+1); 
-			$(Interaction.factor1Span).html(Util.format(Interaction.factor1));
-			$(Interaction.factor2Span).html(Util.format(Interaction.factor2));
+			$(Interaction.factor1Span).html(Interaction.factor1);
+			$(Interaction.factor2Span).html(Interaction.factor2);
 		},
 	isAnswerCorrect : function(value){
 			if(value == Interaction.factor1 * Interaction.factor2)
@@ -103,45 +110,49 @@ var Interaction = {
 			Interaction.pause = true;
 			Interaction.setStatus('Yanlış cevap, doğrusu ' +  Interaction.factor1 * Interaction.factor2 + ' olacaktı',false);
 			Interaction.solutionDiv = $(Interaction.questionDiv).clone().insertAfter(Interaction.questionDiv);
-			var zeros = $(Interaction.factor2Span).html();
-			zeros = '<span class="zero">'+zeros.substring(1,zeros.length)+'</span>';
-			var html = "" + $(Interaction.solutionDiv).html();
-			html = html.substring(0,html.indexOf('<input')) +
-					'<span id="result">' +
-				   (Interaction.factor1*parseInt(Interaction.factor2Span.innerHTML.substring(0,1),10)) + zeros + 
-				   '</span>';
-			
-			$(Interaction.solutionDiv)
-				.html(html)
-				.css({
-					top:$(Interaction.solutionDiv).position().top+60
-				});
-			$('#result',Interaction.solutionDiv)
-				.css({opacity:0})
-				.delay(2000)
-				.animate({opacity:1},1000);
-			$('#factor2',Interaction.solutionDiv)
-				.html(Interaction.factor2Span.innerHTML.substring(0,1)+zeros )
-				.css({opacity:0})
-				.delay(1000)
-				.animate({opacity:1},1000);
-			$('#factor2 .zero',Interaction.solutionDiv)
-				.css({color:'#000'})
-				.delay(3000)
-				.animate(
-					{color:'#f00'},
-					1000
-				);
-			$('.zero',Interaction.solutionDiv)
-				.css({color:'#000'})
-				.delay(3500)
-				.animate(
-					{color:'#f00'},
-					1000,
-					function(){
-						Interaction.pause = false;
-					}
-				);
+            var zeros = $('#factor2',Interaction.questionDiv).html();
+			zeros = zeros.substring(1,zeros.length); 
+			//zeros = zeros.substring(1,zeros.length); 
+			SingleLineMultiply(new Point(160,200),10,zeros,Interaction.factor1,(""+Interaction.factor2).substring(0,1),interactionTextStyle);
+//			var zeros = $(Interaction.factor2Span).html();
+//			zeros = '<span class="zero">'+zeros.substring(1,zeros.length)+'</span>';
+//			var html = "" + $(Interaction.solutionDiv).html();
+//			html = html.substring(0,html.indexOf('<input')) +
+//					'<span id="result">' +
+//				   (Interaction.factor1*parseInt(Interaction.factor2Span.innerHTML.substring(0,1),10)) + zeros + 
+//				   '</span>';
+//			
+//			$(Interaction.solutionDiv)
+//				.html(html)
+//				.css({
+//					top:$(Interaction.solutionDiv).position().top+60
+//				});
+//			$('#result',Interaction.solutionDiv)
+//				.css({opacity:0})
+//				.delay(2000)
+//				.animate({opacity:1},1000);
+//			$('#factor2',Interaction.solutionDiv)
+//				.html(Interaction.factor2Span.innerHTML.substring(0,1)+zeros )
+//				.css({opacity:0})
+//				.delay(1000)
+//				.animate({opacity:1},1000);
+//			$('#factor2 .zero',Interaction.solutionDiv)
+//				.css({color:'#000'})
+//				.delay(3000)
+//				.animate(
+//					{color:'#f00'},
+//					1000
+//				);
+//			$('.zero',Interaction.solutionDiv)
+//				.css({color:'#000'})
+//				.delay(3500)
+//				.animate(
+//					{color:'#f00'},
+//					1000,
+//					function(){
+//						Interaction.pause = false;
+//					}
+//				);
 		}
 }
 
