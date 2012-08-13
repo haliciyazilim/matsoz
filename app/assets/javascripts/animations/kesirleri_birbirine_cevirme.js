35931433c0d76ffc33eb770f3d475598d59dd5a2
Alperen Kavun <alperen@halici.com.tr>
2012-08-13 17:00:50 +0300

    little bug fix

--- app/assets/javascripts/animations/kesirleri_birbirine_cevirme.js
+++ app/assets/javascripts/animations/kesirleri_birbirine_cevirme.js
@@ -947,7 +947,7 @@ var Animation = {
 				},
 				duration: 500,
 				delay: 26500,
-				animationType: 'easeInEaseOut'
+				animationType: 'easeInEaseOut',
                 callback: function(){
                     Main.animationFinished();
                 }
