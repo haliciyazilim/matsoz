e9470a32d64ba02f64e21a8c4518345a53dc6c96
Abdullah Karacabey <abdullah@halici.com.tr>
2012-08-15 09:53:39 +0300

    .

--- app/assets/javascripts/animations/dogal_sayilarla_cikarma_islemi.js
+++ app/assets/javascripts/animations/dogal_sayilarla_cikarma_islemi.js
@@ -37,7 +37,7 @@ Animation.init=function(container){
 	//var islem=new LongSubtraction(20000,19999,"ornek");
 	//var islem=new LongSubtraction(98039,78239,"ornek");
 	var islem=new LongSubtraction(93.57,78.25,"ornek",30);
-	//var islem=new LongSubtraction(7390,5504,"ornek");
+	//var islem=new LongSubtraction(2.45,0.80,"ornek");
 		islem.doldur();
 		islem.basla(1000,1000);
                 
