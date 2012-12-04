8f15911a8678a57f7c399f916c8ef45ba08c62ba
Eren Halici <eren@halici.com.tr>
2012-12-04 15:43:46 +0200

    Merge from development

--- script/export.sh
+++ script/export.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# cp -R app/assets/images/* tmp/export/assets/
-# cp -R app/assets/javascripts/* tmp/export/assets/
-# cp -R app/assets/stylesheets/* tmp/export/assets/
+cp -R app/assets/images/* tmp/export/assets/
+cp -R app/assets/javascripts/* tmp/export/assets/
+cp -R app/assets/stylesheets/* tmp/export/assets/
+cp tmp/jquery.js tmp/assets/
+cp tmp/jquery_ujs.js tmp/assets/
 
-cp -R public/assets tmp/export/
+# cp -R public/assets tmp/export/
 
 find tmp/export -name *.js -o -name *.css -o -name *.html | xargs sed -i.old 's/\/assets/..\/assets/g'
 find tmp/export -name index.html | xargs sed -i.old 's/..\/assets/assets/g'
