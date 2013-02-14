08e87bd362d611802900e2c8ca4cd9cc93918fc1
Yunus Eren Guzel <exculuber@gmail.com>
2013-02-14 10:33:42 +0200

    previous button is added but a new image is required

--- app/assets/javascripts/animations/ataturk_ve_matematik.js
+++ app/assets/javascripts/animations/ataturk_ve_matematik.js
@@ -175,6 +175,22 @@ Interaction.init = function(container){
 		width:103,
 		textAlign:'center'
 	});
+
+    Interaction.previous_button = document.createElement('input');
+    Interaction.previous_button.onclick = Interaction.previousQuestion;
+    $(container).append(Interaction.previous_button);
+    Interaction.previous_button.type = 'button';
+    Interaction.previous_button.className = 'next_button';
+    $(Interaction.previous_button).css({
+        position:'absolute',
+        bottom:'10%',
+        left:'10%',
+        width:103,
+        textAlign:'center'
+    });
+
+
+
 	Interaction.preventNextQuestion = false;
 	var NUMBER_OF_SHAPES  = Interaction.words.length;
 	Interaction.shuffledArray = Util.getShuffledArray(NUMBER_OF_SHAPES);
@@ -189,14 +205,28 @@ Interaction.nextQuestion = function(){
 		return;
 	Interaction.preventNextQuestion = false;
 	project.activeLayer.removeChildren();
-	var count = (Interaction.count++)%Interaction.shuffledArray.length;
-	var wordOrder = Interaction.shuffledArray[count];
+    Interaction.count = (++Interaction.count+Interaction.shuffledArray.length)%Interaction.shuffledArray.length;
+	var wordOrder = Interaction.shuffledArray[Interaction.count];
 	///*TEST*/wordOrder=3;/*TEST*/
 	var word = Interaction.words[wordOrder];
 	$(Interaction.old_word).html(word.oldName);
 	$(Interaction.new_word).html(word.newName);
 	$(Interaction.image).html('').append(word.image);
 	
+}
+
+Interaction.previousQuestion = function(){
+    if(Interaction.preventNextQuestion == true)
+        return;
+    Interaction.preventNextQuestion = false;
+    project.activeLayer.removeChildren();
+    Interaction.count = (--Interaction.count+Interaction.shuffledArray.length)%Interaction.shuffledArray.length;
+    var wordOrder = Interaction.shuffledArray[Interaction.count];
+    ///*TEST*/wordOrder=3;/*TEST*/
+    var word = Interaction.words[wordOrder];
+    $(Interaction.old_word).html(word.oldName);
+    $(Interaction.new_word).html(word.newName);
+    $(Interaction.image).html('').append(word.image);
 }
 
 Interaction.words = [
